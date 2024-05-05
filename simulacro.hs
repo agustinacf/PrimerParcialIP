3a28599f53a36a905431532adc121f6278030554
agustinacf <cf_agustina@hotmail.com>
2024-05-05 16:08:04 -0300

    arreglos

--- simulacro.hs
+++ simulacro.hs
@@ -39,6 +39,7 @@ esIgual x1 x2 | x1 == x2 = True
               | otherwise = False  
 
 pertenece :: String -> (String, String) -> Bool
+pertenece x [] = False
 pertenece x (y1,y2) | x == y1 || x == y2 = True
                     | otherwise = False
 
